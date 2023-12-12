dd94b04be978ba8ccd68d877ea2bd7772a729877
Oladunsi <okeoladunsi6@gmail.com>
2023-12-12 11:59:44 +0100

    added new line at the end of file

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['Soma', 'Union Square'];
+
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
-    };
+  };
 }
