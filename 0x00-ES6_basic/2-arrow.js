c9618af8780cd342d152255ce91d04c97f66f7de
Oladunsi <okeoladunsi6@gmail.com>
2023-12-12 12:30:17 +0100

    correct typo error in 2 arrow file

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,5 +1,5 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['Soma', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
