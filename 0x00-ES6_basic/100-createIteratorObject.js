338bea817432ae32871e251cd94b4b979319ea38
Oladunsi <okeoladunsi6@gmail.com>
2023-12-13 04:59:59 +0100

    cfixed lint suggestions

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   let result = [];
-  for (const value of Object.values(report.allEmployees)){
+  for (const value of Object.values(report.allEmployees)) {
     result = [...result, ...value];
   }
   return result;
