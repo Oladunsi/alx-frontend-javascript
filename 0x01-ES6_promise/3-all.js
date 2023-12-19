eb3e984f47949022ddbce63047622c73f391adb7
Oladunsi <okeoladunsi6@gmail.com>
2023-12-19 16:10:51 +0100

    misspelt system as sytem in 3-all file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    }).catch(() => console.log('Signup sytem offline'));
+    }).catch(() => console.log('Signup system offline'));
 }
